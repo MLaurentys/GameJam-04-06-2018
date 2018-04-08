e3d0a89ac535d0cdceaba59b619bc61a2f0abb5d
MLaurentys <matheus_t.laurentys@hotmail.com>
2018-04-07 23:15:41 -0400

    nightscene - dialogue

--- Assets/Scripts/Interactables.cs
+++ Assets/Scripts/Interactables.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Interactables : MonoBehaviour {
-	public GameObject snore;
+	GameObject snore;
 
 	// Use this for initialization
 	void Start () {
