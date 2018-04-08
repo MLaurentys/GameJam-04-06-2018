4abbd0be5b5e970669b9b68c05145c63b5791fe6
hyliabook <hyliabook@gmail.com>
2018-04-07 20:03:13 -0400

    Dialouge

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    public string[][] oldManMess1;
+    public string[][] cookMess1;
+    public string[][] artistMess1;
+
+    public string[][][] oldManMess;
+    public string[][][] artistMess;
+    public string[][][] cookMess;
+
+    public string[][][][] allMess;
+
+    public int day = 1;
+
+
     public enum GameState{Free,Talking}
 
     public GameState gameState;
@@ -11,6 +24,32 @@ public class GameManager : MonoBehaviour {
     private void Awake()
     {
         gameState = GameState.Free;
+        oldManMess1 = new string[][]{
+            new string[] {"Hey kid, you wouldn't have happened to see something shiny, would you?"},
+            new string[]{ "Mori, I'm going to find our lost treasure... together." },
+            new string[]{"Hey kid, what do you call bears without bees?", "Ears!", "Haha, get it?  Because Bs..."}
+        };
+
+        artistMess1 = new string[][]{
+
+            new string[]{"Hey kid, what's your favoirte cartoon?", "Oh really?", "Ah well,  it makes sense.  I wasn't really expecting anything..."},
+            new string[]{""}
+
+        };
+
+        cookMess1 = new string[][]
+        {
+            new string[]{"Tainted, they says?", "I'll show thems tainted!"},
+            
+        };
+
+        oldManMess = new string[][][] { oldManMess1 };
+        artistMess = new string[][][] { artistMess1 };
+        cookMess = new string[][][]{cookMess1}; 
+        
+
+        allMess = new string[][][][] { oldManMess, artistMess, cookMess };
+
     }
 
     // Use this for initialization
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       		
+	}
+
+    public void Quit()
+    {
+        print("Quitter");
+        Application.Quit();
+    }
+
+    public void Play()
+    {
+        print("Play");
+        SceneManager.LoadScene(2);
+    }
+
+    public void Credits()
+    {
+        print("Credits");
+    }
+}
