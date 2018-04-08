20c879d93072f6e2e9c45f2738c378fdf1aa9e06
hyliabook <hyliabook@gmail.com>
2018-04-07 21:14:16 -0400

    Memento Mori

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -28,19 +28,23 @@ public class GameManager : MonoBehaviour {
             new string[] {"Hey kid, you wouldn't have happened to see something shiny, would you?"},
             new string[]{ "Mori, I'm going to find our lost treasure... together." },
             new string[]{"Hey kid, what do you call bears without bees?", "Ears!", "Haha, get it?  Because Bs..."}
+
         };
 
         artistMess1 = new string[][]{
 
             new string[]{"Hey kid, what's your favoirte cartoon?", "Oh really?", "Ah well,  it makes sense.  I wasn't really expecting anything..."},
-            new string[]{""}
+            new string[]{"(...How am I supposed to finish in just a week?)", "(Are they out of their minds?)"},
+            new string[]{"\"Don't go to art school\"", "\"You'll never make anything out of yourself like that!\"", "Honestly, at this point, I feel as if I should have my father's advice..."}
+       
 
         };
 
         cookMess1 = new string[][]
         {
             new string[]{"Tainted, they says?", "I'll show thems tainted!"},
-            
+            new string[]{"Sigh...", "Kid, don'ts take anythings for granted in this heres life.", "You never knows whose'za gonna screw yous over..."},
+            new string[]{"Hey, I'm walkin' here!"}
         };
 
         oldManMess = new string[][][] { oldManMess1 };
---
+++ Assets/Scripts/GoHoM.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoHoM : Interactables {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+    public override void highlight()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = Color.yellow;
+    }
+
+    public override void triggerInteraction()
+    {
+        print("Memento Mori");
+    }
+
+    public void Yes()
+    {
+
+    }
+
+    public void No()
+    {
+
+    }
+}
